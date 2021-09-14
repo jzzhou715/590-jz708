696dc2a9327341cec73338a7ec3b17b228214523
jzzhou715 <61242291+jzzhou715@users.noreply.github.com>
2021-09-14 13:35:45 -0400

    HW Solution

--- HW1.1/HW1.1/RUN_ALL.sh
+++ HW1.1/HW1.1/RUN_ALL.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-python LogReg.py
-python SimpleLinearReg.py
-python LR.py
+# python LogReg.py
+# python SimpleLinearReg.py
+# python LR.py
+
+python HW1.1-SciPy-BASIC.py
+python HW1.1-SciPy-OPTIMIZER-REGRESSION-1-MODEL-BEST-VERSION.py
+python HW1.1-SciPy-OPTIMIZER-REGRESSION-3-MODELS.py
